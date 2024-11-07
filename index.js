8257ccfde6d68e9655bab013becc3f8b829818a3
Tirth Patel <tirthpatel23t@gmail.com>
2024-11-07 22:56:35 +0530

    update

--- index.js
+++ index.js
@@ -2,25 +2,58 @@ const express = require("express");
 
 // create an express app
 const app = express();
+
+/* 
+when we sent post request from client with data in json format, in req.body it is received as undefined
+To resolve that we need to use app.use(express.json()) in start of file. express.json() function is a built-in middleware in
+Express. Its main job is to parse incoming JSON data from the client
+and make it available in req.body
+*/
 app.use(express.json());
+
+/* 
+express.static is a middleware function that serves static files such as HTML, CSS, JavaScript, images, and more.
+It is often used to serve client-side assets, making it easy to host static files like HTML, CSS, and JavaScript for your web application.
+The root parameter specifies the root directory from which to serve static files.
+The optional options object allows you to configure various settings, such as caching and file handling. 
+
+To access files inside static("folder") in browser by navigating to http://localhost:3000/index.html, http://localhost:3000/sample.jpg
+*/
 app.use(express.static("public"));
+
+/* 
+express.urlencoded is a middleware function that parses incoming URL-encoded data from forms 
+and makes it available in the req.body property 
+*/
 app.use(express.urlencoded({ extended: true }));
 
+/* 
+next() inside this middleware, you're telling Express to proceed to the next handler in the line for the current request.
+This "next handler" could be another middleware or a route handler
+To ensure your logger middleware runs for every request, you should place app.use(loggerMiddleware) before any route handlers 
+*/
 const loggerMiddleWare = (req, res, next) => {
 	console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
 	next(); // call the next middleware
 };
 
+/* this will be called for every incoming request */
 // app.use(loggerMiddleWare);
 
-// define a route
-app.get("/", (req, res) => {
-	res.send("Hello, Express!!!");
+/* this will give error for every route since it is written first, order of middleware and handlers matters */
+app.use((req, res) => {
+	res.status(404).send("Page not found");
 });
+
+// define a route
+// app.get("/", (req, res) => {
+// 	res.send("Hello, Express!!!");
+// });
 
 app.get("/about", (req, res) => {
 	res.send("this is about page");
 });
+
 
 app.post("/data", (req, res) => {
 	console.log("request recieved", req.body);
@@ -32,6 +65,13 @@ const users = [
 	{ id: 2, name: "Doe" },
 ];
 
+/* 
+endpoint to add new user
+send a POST request to http://localhost:3000/users with below JSON body
+{
+  "name": "New User"
+}
+*/
 app.post("/users", (req, res) => {
 	const newUser = req.body;
 	const userID = users.length + 1;
@@ -39,9 +79,30 @@ app.post("/users", (req, res) => {
 	users.push(newUser);
 	res.status(201).json({ message: "User created", user: newUser });
 });
+/* 
+route parameters and query parameters are mechanisms for passing data to a web server or an application, typically through a URL
+They allow you to customize the behavior of your routes and pass data between clients and servers effectively.
 
-// query paramters: www.api.com/users?name=John
+Query parameters are part of the URL's query string and are used to provide additional information or data to the server. 
+They are typically specified after the ? character in the URL and are in the form of key-value pairs
+Send post request with query paramters: www.api.com/users?name=John
+GET /search?q=keyword&page=2&sort=desc
+In Params tab in Postman, parameters are captured 
+*/
 
+
+/* 
+Route parameters are part of the URL's path and are used to define variable parts of a route. 
+They are typically denoted by placeholders in the route pattern, surrounded by colons (:).
+When a client makes a request with a URL that matches the route pattern, the values specified in the URL are extracted 
+and made available to the server or application.
+
+RESTful API for retrieving a specific user's profile: GET /users/:userId
+In this URL, :userId is a route parameter. 
+When a request is made to /users/123, the server can extract the value 123 from the URL and use it to retrieve the user with that ID.
+
+send a DELETE request to http://localhost:3000/users/1 to delete the user with ID 1
+*/
 app.delete("/users/:id", (req, res) => {
 	const userId = parseInt(req.params.id);
 	// find the user with id
@@ -53,6 +114,7 @@ app.delete("/users/:id", (req, res) => {
 	res.json({ message: "User deleted" });
 });
 
+/* apply loggerMiddleware only to a specific route */
 app.get("/special", loggerMiddleWare, (req, res) => {
 	res.send("this is special route");
 });
@@ -63,6 +125,7 @@ app.get("/search", (req, res) => {
 	res.send(`YOur parameters are ${JSON.stringify(querParams)}`);
 });
 
+/* if no route matches or user enters a random path then a page not found message from the server should be sent */
 app.use((req, res) => {
 	res.status(404).send("Page not found");
 });
--- readme.md
+++ readme.md
@@ -5,9 +5,7 @@
 ### Each endpoint corresponds to a specific URL path and usually performs a particular action when accessed
 
 ## Express
-### Express.js is a minimalist web framework for Node.js. While you can certainly build web servers and applications 
-using just the built-in HTTP module in Node.js, Express.js provides a higher-level way 
-to to do certain tasks like manage routes, requests, and views.
+### Express.js is a minimalist web framework for Node.js. While you can certainly build web servers and applications using just the built-in HTTP module in Node.js, Express.js provides a higher-level way to to do certain tasks like manage routes, requests, and views.
 ### This is same like how we can do all our front end development using vanilla js but with React, angular and other lib and framework, we end up writing less code and getting more functionality.
 1. Simpler and cleaner syntax
 2. Code readability
@@ -24,7 +22,26 @@ npm init -y
 npm i express --save
 ```
 
-## Testin Postman
+## Test in Postman
 ### Open Postman and click the "New" button to create a new request. Choose the HTTP method (e.g., GET, POST) for your request.
 ### Enter the URL of the API endpoint you want to test in the request URL field.
 ### You can set request headers by clicking on the "Headers" tab. Headers are used to pass additional information to theserver, such as authentication tokens or content type.
+### If your request requires a request body (e.g., for POST or PUT requests), you can define it in the "Body" tab. You can send data in various formats like JSON, form-data, x-www-form-urlencoded, etc.
+
+## Middleware
+### Middleware functions run between receiving the request and sending the response. Middleware functions in Express.js are functions that have access to the request (req) and response (res) objects and can perform actions or transformations on them They can perform tasks like parsing data, handling authentication, logging, and more. app.use() is a method in Express that is used to apply middleware functions to our application.
+### When we use app.use(), you are essentially telling Express to use a particular middleware function. This function will be called for every incoming request, unless you specify a path to limit its scope
+### if it is written at start of file. all req-res method will use this. if applied to specific routes using app.use() or app.METHOD() (e.g., app.get(), app.post()) particular method app.METHOD() (e.g., app.get(), app.post()), then it can be passed as arg in that method.
+###  In the world of web development, especially with frameworks like Express.js in Node, "middleware" are just like checkpoints. When a request comes into your application, it doesn't just jump straight to the final destination. It often goes through several "checkpoints" or "middlewares" that can check the request, transform it, log it, validate it, and more.
+### So, in simple terms, middleware functions are the individual "checkpoints" in your application that requests go through before reaching their final destination
+
+## PUT
+### This method is used when you want to update a complete resource. When you make a PUT request, you provide a complete updated object. The server then replaces the existing resource withthe provided object. 
+### If certain attributes are missing in the request, those are typically set to their default values or removed. Essentially, a PUT request entirely replaces the existing resource with a new version.
+### Use PUT when you have the complete updated state of the resource. PUT is idempotent, meaning that making the same request multiple times will result in the same state of the resource on the server.
+
+## PATCH
+### PATCH is used for partial updates. With a PATCH request, you only need to provide the specific changes to the resource, not the complete resource. The server then applies these changes to the existing resource.
+### Use PATCH for updating parts of the resource or for situations where sending the complete resource is not feasible or necessary. PATCH can be idempotent, but it's not a requirement. It depends on how the server processes the PATCH request.
+
+
